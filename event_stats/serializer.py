85566a203c68abf47a4a66d66e73b782d0bce7e8
unk217 <unk00@hotmail.com>
2025-06-18 19:17:54 -0500

    update serializer event

--- event_stats/serializer.py
+++ event_stats/serializer.py
@@ -4,4 +4,17 @@ from event_stats.models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = (
+            'id',
+            "round",
+            'event',
+            'çountry',
+            'circuit',
+            'city',
+            'date',
+            'first_gp',
+            'number_laps',
+            'circuit_lenght',
+            'race_distance',
+            'lap_record',
+            )
