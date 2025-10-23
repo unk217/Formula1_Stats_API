7d653161530660b620a749725537c087245be384
unk217 <unk00@hotmail.com>
2025-10-23 16:47:51 -0500

    ordering add

--- drivers_stats/views.py
+++ drivers_stats/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 from drivers_stats.serializer import DriverSerializer
 from drivers_stats.models import Drivers
 
@@ -6,6 +7,7 @@ from drivers_stats.models import Drivers
 class DriverViewSet(ModelViewSet):
     queryset = Drivers.objects.all()
     serializer_class = DriverSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering = ('-season_points',)
     http_method_names = ['get']
-
 
